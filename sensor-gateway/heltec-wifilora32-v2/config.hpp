4925acc18aebb3861b5e31d5ebaf28ada6e6d26e
Marco Piccolino <marco.piccolino@teal.blue>
2022-04-09 00:31:41 +0200

    [sensor-gateway] add config.hpp

---
+++ sensor-gateway/heltec-wifilora32-v2/config.hpp
@@ -0,0 +1,9 @@
+#ifndef DEFINES_HPP
+#define DEFINES_HPP
+
+#define SSID_NAME "your_wifi_network_name"
+#define SSID_PASSWORD "your_wifi_network_password"
+#define SPRINKLER_IP "your Open Sprinkler Bee IP address"
+#define SPRINKLER_KEY "your Open Sprinkler Bee key"
+
+#endif // DEFINES_HPP
